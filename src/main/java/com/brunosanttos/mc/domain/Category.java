5a2708b073ee6ce3e65e0c994e87c84e37c5c9d8
Bruno Santos <brunosanttos@hotmail.com.br>
2023-01-05 11:59:32 -0300

    H2 database and automatic database creation

--- src/main/java/com/brunosanttos/mc/domain/Category.java
+++ src/main/java/com/brunosanttos/mc/domain/Category.java
@@ -3,9 +3,19 @@ package com.brunosanttos.mc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "id_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	
