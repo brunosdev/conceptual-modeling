b36ca333f44f8d952d360603215c84b826773263
Bruno Santos <brunosanttos@hotmail.com.br>
2023-01-05 12:38:19 -0300

    Creating instantiation operation

--- src/main/java/com/brunosanttos/mc/McApplication.java
+++ src/main/java/com/brunosanttos/mc/McApplication.java
@@ -1,13 +1,32 @@
 package com.brunosanttos.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.brunosanttos.mc.domain.Category;
+import com.brunosanttos.mc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class McApplication {
+public class McApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
 	}
 
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Office");
+		Category cat2 = new Category(null, "Tools");
+		Category cat3 = new Category(null, "Computing");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+	}
 }
