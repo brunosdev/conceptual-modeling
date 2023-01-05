cc1bfd5498253682c854d98b2a201698b983ede4
Bruno Santos <brunosanttos@hotmail.com.br>
2023-01-05 11:46:05 -0300

    Testing the first domain class - Category

--- src/main/java/com/brunosanttos/mc/resources/CategoryResource.java
+++ src/main/java/com/brunosanttos/mc/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.brunosanttos.mc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brunosanttos.mc.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@GetMapping
-	public String list() {
-		return "Rest is working!";
+	public List<Category>list() {
+		Category cat1 = new Category(1, "Office");
+		Category cat2 = new Category(2, "TV & Video");
+		
+		List<Category> ls = new ArrayList<>();
+		
+		ls.add(cat1);
+		ls.add(cat2);
+			
+		return ls;
 	}
 }
